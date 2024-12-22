9c244f37fa8b323db43dbdd0f654e45bc26f9d6d
gudfhr95 <ghldnjsdlf@gmail.com>
2024-12-22 17:33:56 +0900

    chore: add lint

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,37 +1,88 @@
 plugins {
     kotlin("jvm")
+    kotlin("plugin.allopen")
 
-    kotlin("plugin.spring")
-    id("org.springframework.boot")
-    id("io.spring.dependency-management")
+    id("org.jlleitschuh.gradle.ktlint")
+    id("io.gitlab.arturbosch.detekt")
+    id("jacoco")
 }
 
-group = "io.openads"
-version = "0.0.1-SNAPSHOT"
+allprojects {
+    apply(plugin = "kotlin")
+    apply(plugin = "kotlin-allopen")
 
-repositories {
-    mavenCentral()
-}
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+    apply(plugin = "io.gitlab.arturbosch.detekt")
+    apply(plugin = "jacoco")
 
-dependencies {
-}
+    group = "io.openads"
+    version = "0.0.1"
 
-testing {
-    suites {
-        val test by getting(JvmTestSuite::class) {
-            useKotlinTest("2.1.0")
+    repositories {
+        mavenCentral()
+    }
+
+    dependencies {
+        implementation("org.jetbrains.kotlin:kotlin-reflect")
+        testImplementation(Kotlin.test.junit5)
+        testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    }
+
+    java {
+        toolchain {
+            languageVersion = JavaLanguageVersion.of(21)
         }
     }
-}
 
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
+    kotlin {
+        compilerOptions {
+            freeCompilerArgs.addAll("-Xjsr305=strict")
+        }
     }
-}
 
-kotlin {
-    compilerOptions {
-        freeCompilerArgs.addAll("-Xjsr305=strict")
+    ktlint {
+        version = "1.5.0"
+    }
+
+    detekt {
+        config.setFrom(files("$rootDir/detekt.yaml"))
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+
+        finalizedBy("jacocoTestReport")
+    }
+
+    tasks.jacocoTestReport {
+        reports {
+            html.required = true
+            xml.required = false
+            csv.required = false
+        }
+
+        finalizedBy("jacocoTestCoverageVerification")
+    }
+
+    tasks.jacocoTestCoverageVerification {
+        violationRules {
+            rule {
+                element = "CLASS"
+
+                limit {
+                    counter = "BRANCH"
+                    value = "COVEREDRATIO"
+                    minimum = BigDecimal.valueOf(0.70)
+                }
+
+                limit {
+                    counter = "LINE"
+                    value = "COVEREDRATIO"
+                    minimum = BigDecimal.valueOf(0.70)
+                }
+
+                isFailOnViolation = true
+            }
+        }
     }
 }
